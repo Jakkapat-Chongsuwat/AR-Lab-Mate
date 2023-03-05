42eebc72ae2535a8057998498bd6b77a75af8c77
Jakkapat-Chongsuwat <70486906+Jakkapat-Chongsuwat@users.noreply.github.com>
2023-03-06 03:33:39 +0700

    update sh file

--- .github/workflows/PostBuild.sh
+++ .github/workflows/PostBuild.sh
@@ -9,10 +9,10 @@ PLAYER_PATH=$UNITY_PLAYER_PATH
 #IF we are using a Windows Builder and using the path to pass it to a native Windows application we need to properly convert the cygwin player path to windows format
 
 if [[ "$BUILDER_OS" == "WINDOWS" ]]; then
-PLAYER_PATH=$(cygpath -wa "$UNITY_PLAYER_PATH")
+  PLAYER_PATH=$(cygpath -wa "$UNITY_PLAYER_PATH")
 fi
 
 echo "$PLAYER_PATH"
 
-# Set an output variable using the `set-output` command
-echo "::set-output name=ARTIFACT_PATH::$PLAYER_PATH"
+# Write the output to an environment file
+echo "ARTIFACT_PATH=$PLAYER_PATH" >> $GITHUB_ENV
