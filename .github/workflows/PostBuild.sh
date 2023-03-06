bac518dab9767d2be04dce22426e331b3b5a271a
Jakkapat-Chongsuwat <70486906+Jakkapat-Chongsuwat@users.noreply.github.com>
2023-03-06 14:00:56 +0700

    Update PostBuild.sh

--- .github/workflows/PostBuild.sh
+++ .github/workflows/PostBuild.sh
@@ -8,7 +8,7 @@ PLAYER_PATH=$UNITY_PLAYER_PATH
 
 #IF we are using a Windows Builder and using the path to pass it to a native Windows application we need to properly convert the cygwin player path to windows format
 
-if [ "$BUILDER_OS" == "WINDOWS" ]; then
+if [ "$BUILDER_OS" = "WINDOWS" ]; then
   PLAYER_PATH=$(cygpath -wa "$UNITY_PLAYER_PATH")
 fi
 
