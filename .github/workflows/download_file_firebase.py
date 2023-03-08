988cbf557ff71c247cc41ef6d941a76fb6cf3a52
Jakkapat-Chongsuwat <70486906+Jakkapat-Chongsuwat@users.noreply.github.com>
2023-03-08 20:55:34 +0700

    change to use python to download file

---
+++ .github/workflows/download_file_firebase.py
@@ -0,0 +1,40 @@
+import firebase_admin
+from firebase_admin import credentials, storage
+import os
+import sys
+
+# define the bucket name as a variable
+bucket_name = 'metaschoolmeet-ff047'
+
+# define the folder path in firebase storage
+firebaseFolderPath = 'MetaSchoolLiteData/'
+
+# google cloud's service account key file absolute path on github's machine directory
+# note that the file name must be matched with the file name created from timheuer/base64-to-file@v1 action on the workflow
+keyFilePath = '.github/workflows/GOOGLE_SERVICE_KEY.json'
+
+# apply the bucket domain to the credentials
+cred = credentials.Certificate(keyFilePath)
+firebase_admin.initialize_app(cred, {
+    'storageBucket' : f'{bucket_name}.appspot.com'
+})
+
+# refer to the storage bucket
+bucket = storage.bucket()
+
+# get the download file's path in repository's directory
+if len(sys.argv) > 1:
+    fileName = sys.argv[1]
+else:
+    print("Error: File name not specified as a command-line argument.")
+    exit()
+dirname = os.path.dirname(os.path.realpath(__file__))
+fileFullPath = dirname + '/' + fileName
+
+# if the file name contains file path, the bucket will create folders corresponding to the path.
+blob = bucket.blob(firebaseFolderPath + fileName)
+
+# download from firebase storage
+blob.download_to_file(fileFullPath)
+
+print("Downloaded file from Firebase Storage: ", fileFullPath)
